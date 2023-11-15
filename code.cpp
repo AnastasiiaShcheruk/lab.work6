5dbc8a369cd377c429469c312d755450e6aac9ba
Anastasiia <nastay.sheruk05@gmail.com>
2023-11-15 16:46:02 +0200

    New file with code added

---
+++ code.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <Windows.h>
+#include <iomanip>
+#define _USE_MATH_DEFINES
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    float Z, dob, y;
+    cout << "Введіть y:\ny = ";
+    cin >> y;
+    dob = 1;
+    if (pow(y,2)+3==0) {
+        cout << "Помилка";
+    }
+    for (int i = 1; i <= 5; i++) {
+        dob *= i;
+    }
+    Z = (dob + 2 * cos(y)) / (pow(y, 2) + 3); 
+    cout << setw(6) << "Z = " << setprecision(0) << Z;
+}
